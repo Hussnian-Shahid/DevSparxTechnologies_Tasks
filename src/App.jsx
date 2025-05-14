fb3891ab46b80eda1beb17f0bd2d49d41eae842f
Hussnain-shahid <your.actual.hussnainshahid1202@gmail.com>
2025-05-14 14:52:59 +0500

    sdfjkls

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import React from 'react'
-import Navbar from './Component/Navbar'
 import Note from './Component/Note'
 
 const App = () => {
   return (
     <>
-    {/* <Navbar/> */}
     <Note/>
     </>
   )
